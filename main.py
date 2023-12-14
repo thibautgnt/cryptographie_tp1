58af804d113b04dc7ed546f94a9298db1a36f7ef
thibautgnt <thibaut0genet@gmail.com>
2023-12-14 15:21:16 +0100

    Created readme.md

---
+++ main.py
@@ -0,0 +1,48 @@
+def xor(a, b):
+    return ''.join(chr(ord(x) ^ ord(y)) for x, y in zip(a, b))
+
+def cbc_encrypt(data, key, iv):
+    encrypted = ""
+    previous_block = iv
+
+    for i in range(0, len(data), len(key)):
+        block = data[i:i+len(key)]
+        block = xor(block, previous_block)
+        encrypted += block
+        previous_block = block
+
+    return encrypted
+
+# Encrypt
+data = "EfreiParis"
+key = "HNY5PfXZ"
+iv = "EpitaParis"
+
+encrypted_data = cbc_encrypt(data, key, iv)
+print("Données chiffrées:", encrypted_data)
+
+def cbc_decrypt(data, key, iv):
+    decrypted = ""
+    previous_block = iv
+
+    for i in range(0, len(data), len(key)):
+        block = data[i:i+len(key)]
+        decrypted += xor(block, previous_block)
+        previous_block = block
+
+    return decrypted
+
+# Decrypt
+decrypted_data = cbc_decrypt(encrypted_data, key, iv)
+print("Données déchiffrées:", decrypted_data)
+
+def cbc_decrypt(data, key, iv):
+    decrypted = ""
+    previous_block = iv
+
+    for i in range(0, len(data), len(key)):
+        block = data[i:i+len(key)]
+        decrypted += xor(block, previous_block)
+        previous_block = block
+
+    return decrypted
