4458df11d064e3af461e951cd087950bcabb972b
thibautgnt <thibaut0genet@gmail.com>
2023-12-21 20:18:16 +0100

    Too much comments

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 # Fonction pour effectuer l'opération XOR entre deux chaînes.
 def xor(a, b):
-    # Convertit les caractères des chaînes en valeurs ASCII, effectue XOR entre chaque paire de caractères,
-    # et reconvertit le résultat en caractère. Le tout est assemblé en une seule chaîne.
     return ''.join(chr(ord(x) ^ ord(y)) for x, y in zip(a, b))
 
 # Fonction pour chiffrer les données en utilisant le mode CBC.
 def cbc_encrypt(data, key, iv):
-    encrypted = ""  # Chaîne pour stocker le résultat chiffré.
-    previous_block = iv  # Initialise avec le vecteur d'initialisation (IV).
+    encrypted = ""
+    previous_block = iv
 
     # Traite le message par blocs de la taille de la clé.
     for i in range(0, len(data), len(key)):
-        block = data[i:i+len(key)]  # Sélectionne un bloc de données.
-        block = xor(block, previous_block)  # Chiffre le bloc avec XOR en utilisant le bloc précédent.
-        encrypted += block  # Ajoute le bloc chiffré au résultat.
-        previous_block = block  # Met à jour le bloc précédent pour le prochain tour.
+        block = data[i:i+len(key)]
+        block = xor(block, previous_block)
+        encrypted += block
+        previous_block = block
 
     return encrypted
 
 # Chiffrement.
-data = "EfreiParis" # Données à chiffrer.
-key = "HNY5PfXZ" # Clé de chiffrement.
-iv = "EpitaParis" # Vecteur d'initialisation (IV).
+data = "EfreiParis"
+key = "HNY5PfXZ"
+iv = "EpitaParis"
 
-encrypted_data = cbc_encrypt(data, key, iv) # Chiffre les données.
-print("Données chiffrées:", encrypted_data) # Affiche le résultat chiffré.
+encrypted_data = cbc_encrypt(data, key, iv)
+print("Données chiffrées:", encrypted_data)
 
 # Fonction pour déchiffrer les données en utilisant le mode CBC.
 def cbc_decrypt(data, key, iv):
-    decrypted = ""  # Chaîne pour stocker le résultat déchiffré.
-    previous_block = iv  # Initialise avec le vecteur d'initialisation (IV).
+    decrypted = ""
+    previous_block = iv
 
     # Traite les données chiffrées par blocs.
     for i in range(0, len(data), len(key)):
-        block = data[i:i+len(key)]  # Sélectionne un bloc de données chiffrées.
-        decrypted += xor(block, previous_block)  # Déchiffre le bloc avec XOR en utilisant le bloc précédent.
-        previous_block = block  # Met à jour le bloc précédent pour le prochain tour.
+        block = data[i:i+len(key)]
+        decrypted += xor(block, previous_block)
+        previous_block = block
 
     return decrypted
 
 # Déchiffrement.
-decrypted_data = cbc_decrypt(encrypted_data, key, iv) # Déchiffre les données.
-print("Données déchiffrées:", decrypted_data) # Affiche le résultat déchiffré.
+decrypted_data = cbc_decrypt(encrypted_data, key, iv)
+print("Données déchiffrées:", decrypted_data)
